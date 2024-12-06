2485b2b148926d4593f6d903825b851fe2c9fd07
JamesBurnsBacon <cookingbaconwithfire@gmail.com>
2024-12-05 23:55:54 -0500

    Setup

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import MevShareClient, {IPendingTransaction} from '@flashbots/mev-share-client'
+//Listen for new pending tx
+import { Contract, JsonRpcProvider, Wallet } from 'ethers'
+//Create and sign our tx, query blockchain
